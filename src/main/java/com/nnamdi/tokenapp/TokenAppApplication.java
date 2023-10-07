12b342f722234b9b435544446d9125d217a54b5f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-07 23:56:03 +0200

    Initial commit

---
+++ src/main/java/com/nnamdi/tokenapp/TokenAppApplication.java
@@ -0,0 +1,13 @@
+package com.nnamdi.tokenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenAppApplication.class, args);
+	}
+
+}
