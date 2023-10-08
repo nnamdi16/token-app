90653686e70dcbcb1379a47d80ef8b2bb22a1430
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-08 22:19:16 +0200

    feat: Implemented service to generate token

---
+++ generator/src/main/java/com/nnamdi/generator/services/GeneratorService.java
@@ -0,0 +1,5 @@
+package com.nnamdi.generator.services;
+
+public interface GeneratorService {
+    public String generateToken(String pin);
+}
---
+++ generator/src/main/java/com/nnamdi/generator/services/impl/GeneratorServiceImpl.java
@@ -0,0 +1,10 @@
+package com.nnamdi.generator.services.impl;
+
+import com.nnamdi.generator.services.GeneratorService;
+
+public class GeneratorServiceImpl implements GeneratorService {
+    @Override
+    public String generateToken(String pin) {
+        return null;
+    }
+}
