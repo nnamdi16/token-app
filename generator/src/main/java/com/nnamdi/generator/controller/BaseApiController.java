794a562e483f80eabe661bce2cce43c66dae68f1
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-10 01:31:47 +0200

    chore: added a private empty constructor to hide the implicit public one

--- generator/src/main/java/com/nnamdi/generator/controller/BaseApiController.java
+++ generator/src/main/java/com/nnamdi/generator/controller/BaseApiController.java
@@ -1,6 +1,7 @@
 package com.nnamdi.generator.controller;
 
 public abstract class BaseApiController {
+    private BaseApiController() {}
     public static final String BASE_API_PATH = "/api/v1/";
     public static final String GENERATOR= "generator";
 }
