5c41ec972f759d0f1ce3cdfc11ee3e01219f2846
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-10 01:24:07 +0200

    chore: modified the response message

---
+++ validator/src/main/java/com/nnamdi/validator/domain/response/APIResponseMessages.java
@@ -0,0 +1,8 @@
+package com.nnamdi.validator.domain.response;
+
+public class APIResponseMessages {
+
+    private APIResponseMessages() {}
+
+    public  static  final String SUCCESSFUL="successfully validated token";
+}
