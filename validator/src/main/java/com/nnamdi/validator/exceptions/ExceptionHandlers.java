dfa73beb72699d60b988b39d69577dab27e4abe6
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-10 01:33:08 +0200

    chore: Implemented the ExceptionHandlers for the ValidatorService

---
+++ validator/src/main/java/com/nnamdi/validator/exceptions/ExceptionHandlers.java
@@ -0,0 +1,26 @@
+package com.nnamdi.validator.exceptions;
+
+import com.nnamdi.validator.domain.response.AppResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandlers {
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public AppResponse handleBadRequest (final BadRequestException ex) {
+        return AppResponse.builder()
+                .data(null)
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .success(false)
+                .build();
+    }
+}
