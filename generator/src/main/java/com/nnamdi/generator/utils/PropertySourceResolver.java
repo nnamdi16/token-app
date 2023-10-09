e1a45480ffb014baa484d55fc43b3a8863bd5998
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-09 08:32:13 +0200

    chore: Added constructor to PropertySourceResolver

--- generator/src/main/java/com/nnamdi/generator/utils/PropertySourceResolver.java
+++ generator/src/main/java/com/nnamdi/generator/utils/PropertySourceResolver.java
@@ -1,11 +1,13 @@
 package com.nnamdi.generator.utils;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @Getter
+@AllArgsConstructor
 public class PropertySourceResolver {
     @Value("${project.version}")
     private String projectVersion;
