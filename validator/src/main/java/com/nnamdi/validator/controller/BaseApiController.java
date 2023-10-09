9627f9aaa632897467eda071948a06416a900ea6
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-10 01:32:16 +0200

    chore: Implemented the BaseApiController for the ValidatorService

---
+++ validator/src/main/java/com/nnamdi/validator/controller/BaseApiController.java
@@ -0,0 +1,7 @@
+package com.nnamdi.validator.controller;
+
+public class BaseApiController {
+    private BaseApiController() {}
+    public  static  final String BASE_API_PATH="/api/v1/";
+    public static final String VALIDATOR = "validator";
+}
