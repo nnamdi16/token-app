e8773f1783d423b4f689a9f93dc3cc4024df5633
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-09 08:37:14 +0200

    feat: implemented BadRequestException to handle bad request exceptions that are thrown

---
+++ generator/src/main/java/com/nnamdi/generator/exceptions/BadRequestException.java
@@ -0,0 +1,19 @@
+package com.nnamdi.generator.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+
+}
