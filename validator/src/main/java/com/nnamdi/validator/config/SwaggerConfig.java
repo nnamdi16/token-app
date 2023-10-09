982f2e81f316c29d4f59fd652023406abfe91d1e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-10 01:39:08 +0200

    chore: Implemented swagger configuration

---
+++ validator/src/main/java/com/nnamdi/validator/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.nnamdi.validator.config;
+
+import com.nnamdi.validator.utils.PropertySourceResolver;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+
+public class SwaggerConfig {
+    PropertySourceResolver propertySourceResolver;
+
+
+    public SwaggerConfig(PropertySourceResolver propertySourceResolver) {
+        this.propertySourceResolver = propertySourceResolver;
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        Contact contact = new Contact();
+        contact.email("nwabuokeinnamdi19@gmail.com");
+        contact.name("Nwabuokei Nnamdi");
+
+        Info info = new Info()
+                .title(propertySourceResolver.getProjectName())
+                .version(propertySourceResolver.getProjectVersion())
+                .contact(contact)
+                .description(propertySourceResolver.getProjectDescription());
+        return new OpenAPI().info(info);
+
+    }
+}
