7793c3fc9617da8244dbf652d42a2e31f134b829
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-09 08:30:41 +0200

    chore: implemented PropertySourceResolver to manage the properties required in the project and eliminate hard coding.

---
+++ generator/src/main/java/com/nnamdi/generator/utils/PropertySourceResolver.java
@@ -0,0 +1,18 @@
+package com.nnamdi.generator.utils;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class PropertySourceResolver {
+    @Value("${project.version}")
+    private String projectVersion;
+
+    @Value("${project.name}")
+    private String projectName;
+
+    @Value("${project.description}")
+    private String projectDescription;
+}
