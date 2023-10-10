9d23df478d9aec822baecb69cd522df5d8d6b1b1
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-10 08:33:12 +0200

    chore: Removed unused imports

--- generator/src/main/java/com/nnamdi/generator/utils/PropertySourceResolver.java
+++ generator/src/main/java/com/nnamdi/generator/utils/PropertySourceResolver.java
@@ -1,6 +1,5 @@
 package com.nnamdi.generator.utils;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
