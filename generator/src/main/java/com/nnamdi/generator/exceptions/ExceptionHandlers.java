86df77767c849eb7503ecd9e1450a25d407df651
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-12 04:29:19 +0200

    chore: Added custom exception response for field validation

--- generator/src/main/java/com/nnamdi/generator/exceptions/ExceptionHandlers.java
+++ generator/src/main/java/com/nnamdi/generator/exceptions/ExceptionHandlers.java
@@ -3,6 +3,9 @@ package com.nnamdi.generator.exceptions;
 import com.nnamdi.generator.domain.response.AppResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,4 +26,29 @@ public class ExceptionHandlers {
                 .success(false)
                 .build();
     }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public AppResponse processValidationError(MethodArgumentNotValidException ex) {
+
+        BindingResult result = ex.getBindingResult();
+
+        FieldError error = result.getFieldError();
+
+        assert error != null;
+        return processFieldError(error);
+    }
+
+
+    private AppResponse processFieldError(FieldError error) {
+
+        return AppResponse.builder()
+                .error(error.getDefaultMessage())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(error.getDefaultMessage())
+                .build();
+    }
+
 }
