abb03c18263be8c9181912700aa57063eb4cd553
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-10-13 10:34:51 +0200

    chore: refactored validate token component adding the right error message

--- frontend/src/Pages/Token/ValidateToken.tsx
+++ frontend/src/Pages/Token/ValidateToken.tsx
@@ -7,37 +7,36 @@ import { validateToken } from "../../services/token.service";
 import Button from "../../components/Button";
 
 
-document.title="Generate Token"
+document.title = "Generate Token"
 const ValidateToken = () => {
-  const [token, setToken] = useState<string | undefined>(undefined);
+  const [token, setToken] = useState<string>('');
   const [isLoading, setsetIsLoading] = useState(false);
 
   const handleOnchange = (event?: React.ChangeEvent<HTMLInputElement>) => {
-    
     event?.preventDefault();
     // Use a regular expression to remove non-numeric characters
     const numericValue = event?.target.value.replace(/[^0-9]/g, '');
-    setToken(numericValue);
+    setToken(numericValue!!);
   };
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-    
-    if(!token){
-        return toast.error('Enter a valid token');
+
+    if (!token) {
+      return toast.error('Enter a valid token');
     }
     //call end point
     try {
-      setsetIsLoading(prev=>!prev);
-      const isTokenValid=await validateToken(token.trim());
-      setsetIsLoading(prev=>!prev);
-      const {data,error,message,status}=isTokenValid;
-      if(!error && status===200){
+      setsetIsLoading(prev => !prev);
+      const isTokenValid = await validateToken(token.trim());
+      setsetIsLoading(prev => !prev);
+      const { error, message, status } = isTokenValid || {};
+      if (!error && status === 200) {
         setToken('');
         toast.success('Token validated successfully');
         return;
       }
-        toast.error('An error occured while validating the token');
-        return;
+      message ? toast.error(message) : toast.error('An error occurred while validating the token');
+      return;
     } catch (ex) {
       if (ex instanceof Error) {
         toast.error(ex.message);
@@ -48,7 +47,7 @@ const ValidateToken = () => {
     <section className={styles.container}>
       <form onSubmit={handleSubmit} className={styles.container}>
         <h1>Validate Token </h1>
-        <Input value={token!!} onChange={handleOnchange} placeholder="Enter Token" />
+        <Input value={token} onChange={handleOnchange} placeholder="Enter Token" />
         <Button isDisabled={isLoading} text="Validate Token" />
       </form>
     </section>
