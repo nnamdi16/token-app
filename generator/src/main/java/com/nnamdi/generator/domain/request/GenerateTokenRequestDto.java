217758d056b3c3af409401c283c70b70e32391c1
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-11-06 21:49:52 +0100

    chore: added validation to check the number of digits provided in the request.

--- generator/src/main/java/com/nnamdi/generator/domain/request/GenerateTokenRequestDto.java
+++ generator/src/main/java/com/nnamdi/generator/domain/request/GenerateTokenRequestDto.java
@@ -1,6 +1,7 @@
 package com.nnamdi.generator.domain.request;
 
 import com.nnamdi.generator.utils.ValidatePin;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,6 +19,7 @@ public class GenerateTokenRequestDto implements Serializable {
      * Assumption that the maximum number of digits are 5
      */
     @Size(max = 5, min = 5, message = "Pin must be 5 digits")
+    @Pattern(regexp = "\\d+", message = "Only digits are allowed")
     @ValidatePin()
     private String pin;
 }
